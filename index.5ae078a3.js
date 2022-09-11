a04437c0bf8742c5394e2ec219dd42e3166b66d9
Dessed <87389570+Dessed@users.noreply.github.com>
2022-09-11 18:49:12 +0000

    Deploying to gh-pages from @ Dessed/goit-js-hw-10@4454cbc9768434a5847bd9fde6544f0c0e8cb755 🚀

---
+++ index.5ae078a3.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/{name}?fullText=true"),fetch("https://restcountries.com/v3.1/name/aruba?fullText=true");
+//# sourceMappingURL=index.5ae078a3.js.map
---
+++ index.b4f9eed8.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/{name}?fullText=true"),fetch("https://restcountries.com/v3.1/name/aruba?fullText=true");
+//# sourceMappingURL=index.b4f9eed8.js.map
